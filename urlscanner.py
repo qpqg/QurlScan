fbfdffaefd2b21a21a2337f5a1ddfa815c9dd957
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2018-09-11 13:10:04 +0700

    Update urlscanner.py

--- urlscanner.py
+++ urlscanner.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from sys import exit
 import requests
 from modul.color import colorku
 cl = colorku()
@@ -8,10 +9,14 @@ def sit(sites):
         return sites.replace("https://","").replace("http://","")
 def open_list(f):
     site = []
-    with open(f) as files:
-        for list_web in files.readlines():
-            web = list_web.split("\n")[0]
-            site.append(sit(web))
+    try:
+        with open(f) as files:
+            for list_web in files.readlines():
+                web = list_web.split("\n")[0]
+                site.append(sit(web))
+    except IOError:
+        print cl.warning_red("PATH TIDAK DI KETAHUI")
+        exit(0)
     scanning = cl.dflt_C("\n".join([str(i)+". "+site[i] for i in range(len(site))]))
     print cl.dflt_G("[+] ========== [ SITES ] =========== [+]")
     print scanning
@@ -30,8 +35,9 @@ def goto(x):
             print cl.dflt_G(format)
             print cl.dflt_G("STATUS CODE: "+str(code.status_code))
             try:
-                with open(file) as save:
-                    print "File Saved To: {}".format(file)
+                log = "log.txt"
+                with open(log, "a+") as save:
+                    print "File Saved To: {}".format(log)
                     save.write(format + "\n")
                     save.close()
             except:
@@ -60,9 +66,11 @@ def start_all():
     if len(hijau) > 0:
         print "[+] ========== [ HIJO ] =========== [+]"
         print "\n".join([hijau[i] for i in range(len(hijau))])
-    print cl.dflt_end()
+    else:
+        print cl.dflt_R("GA ADA IJO")
 if __name__ == '__main__':
     cl.baleho(40,judul="Simple Url Scanner")
-    f = raw_input(cl.dflt_O("Enter Yout Path Site: ")+'\033[34m')
+    print cl.help_B("input path file, Example: c://site.txt")
+    f = raw_input(cl.dflt_O(" > ")+'\033[34m')
     sit = open_list(f)
     start_all()
